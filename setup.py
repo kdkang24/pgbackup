6d8d5c0c7d5a45f57200db8c92a2a0e848f2dbd8
Kevin Kang <kdkang24@gmail.com>
2020-03-14 18:31:47 -0700

    Initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='pgbackup',
+    version='0.1.0',
+    author='Kevin Kang',
+    author_email='kdkang24@gmail.com',
+    description='A utility for backing up PostgreSQL databases',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/kdkang24/pgbackup',
+    packages=find_packages('src')
+)
